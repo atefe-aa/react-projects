ac5c81000913ac9efde8c48ee1c13d3dc61e6e93
atefe-aa <aa.asgariyan12@gmail.com>
2023-08-02 10:21:37 +0330

    use callback functions to set states

--- src/08-How-React-works/App.js
+++ src/08-How-React-works/App.js
@@ -40,7 +40,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
@@ -64,9 +67,26 @@ function TabContent({ item }) {
   const [likes, setLikes] = useState(0);
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+  function handleTripleInc() {
+    // setLikes(likes + 3);
+
+    //this code below does not work
+    // setLikes(likes +1);
+    // setLikes(likes +1);
+    // setLikes(likes +1);
+
+    //but this one does
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
   }
 
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -80,12 +100,12 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
