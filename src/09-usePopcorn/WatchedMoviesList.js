69fd680ea14b8241daff65f86aae081e15e5fa12
atefe-aa <aa.asgariyan12@gmail.com>
2023-08-14 20:17:06 +0330

    add key prop

--- src/09-usePopcorn/WatchedMoviesList.js
+++ src/09-usePopcorn/WatchedMoviesList.js
@@ -4,7 +4,7 @@ export function WatchedMoviesList({ watched, onDelete }) {
   return (
     <ul className="list">
       {watched?.map((movie) => (
-        <WatchedMovie movie={movie} onDelete={onDelete} />
+        <WatchedMovie movie={movie} onDelete={onDelete} key={movie.imdbID} />
       ))}
     </ul>
   );
