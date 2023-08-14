548bc2e3e6ea5886419edef3aee033a74ee067a2
atefe-aa <aa.asgariyan12@gmail.com>
2023-08-14 14:57:19 +0330

    duplicate the app into two versions to start new practice on the app

---
+++ src/09-usePopcorn/v1/App.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import { Loader } from "./Loader";
+import { ErrorMessage } from "./ErrorMessage";
+import { Navbar } from "./Navbar";
+import { Search } from "./Search";
+import { Logo } from "./Logo";
+import { NumResult } from "./NumResult";
+import { Main } from "./Main";
+import { Box } from "./Box";
+import { MovieList } from "./MovieList";
+import { MovieDetailes } from "./MovieDetailes";
+import { WatchedSummery } from "./WatchedSummery";
+import { WatchedMoviesList } from "./WatchedMoviesList";
+
+document.title = `usePopcorn`;
+
+export const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr?.length, 0);
+
+export const key = "9b708f19";
+
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
+
+  function handleAddWatched(movie) {
+    setWatched((watched) => [...watched, movie]);
+  }
+
+  function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+  }
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function moviesFetching() {
+        try {
+          setIsLoading(true);
+          setError("");
+          //to prevent fetching so many requests at the same time
+          const res = await fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Somthing went wrong with fetching movies!");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found!");
+
+          setMovies(data.Search);
+          setError(""); //because of rerendering and abort errors
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            // to ignor abort errors
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          //to have the is loading off,after everything is done specially after throwing an error
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      handleCloseMovie();
+      moviesFetching();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return (
+    <>
+      <Navbar>
+        <Logo />
+        <Search query={query} setQuery={setQuery} />
+        <NumResult movies={movies} />
+      </Navbar>
+      <Main>
+        <Box>
+          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+          {error && <ErrorMessage message={error} />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelect={handleSelectMovie} />
+          )}
+          {isLoading && <Loader />}
+        </Box>
+
+        <Box>
+          {selectedId ? (
+            <MovieDetailes
+              selectedId={selectedId}
+              onAddWatched={handleAddWatched}
+              onClose={handleCloseMovie}
+              watched={watched}
+            />
+          ) : (
+            <>
+              {" "}
+              <WatchedSummery watched={watched} />
+              <WatchedMoviesList
+                watched={watched}
+                onDelete={handleDeleteWatched}
+              />
+            </>
+          )}
+        </Box>
+      </Main>
+    </>
+  );
+}
---
+++ src/09-usePopcorn/v1/MovieDetailes.js
@@ -0,0 +1,150 @@
+import { useEffect, useState } from "react";
+import StarRating from "../star-rating/App";
+import { Loader } from "./Loader";
+import { key } from "./App";
+
+export function MovieDetailes({ selectedId, onClose, onAddWatched, watched }) {
+  const [movie, setMovie] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [userRating, setUserRating] = useState(0);
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
+  const watchedUserRating = watched.find(
+    (movie) => movie.imdbID === selectedId
+  )?.userRating;
+
+  function handleAdd() {
+    const newWatchedMovie = {
+      imdbID: selectedId,
+      title,
+      year,
+      poster,
+      userRating,
+      imdbRating: Number(imdbRating),
+      runtime: Number(runtime.split(" ").at(0)),
+    };
+
+    onAddWatched(newWatchedMovie);
+    onClose();
+  }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onClose();
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return function () {
+        //to prevent having so many event listners which causes memory problems
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onClose]
+  );
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function getMovieDetailes() {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${key}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false);
+      }
+      getMovieDetailes();
+    },
+    [selectedId]
+  );
+
+  useEffect(
+    function () {
+      //it changes the page title to the selected movie title
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      //to change the page title back to usePopcorn when unselect a movie
+      return function () {
+        //this will execute after rerendering
+        document.title = "usePopcorn";
+      };
+    },
+    [title]
+  );
+
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {" "}
+          <header>
+            <button className="btn-back" onClick={onClose}>
+              &larr;
+            </button>
+            <img src={poster} alt={`poster of ${title}`} />
+            <div className="details-overview">
+              <h2>
+                {title} {year}{" "}
+              </h2>
+              <p>
+                {released}&bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMBD rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              {isWatched ? (
+                <p>
+                  You rated this movie {watchedUserRating} <span>⭐</span>
+                </p>
+              ) : (
+                <>
+                  {" "}
+                  <StarRating
+                    size={24}
+                    maxRating={10}
+                    onStarRating={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      + Add to list
+                    </button>
+                  )}
+                </>
+              )}
+            </div>
+
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
+    </div>
+  );
+}
