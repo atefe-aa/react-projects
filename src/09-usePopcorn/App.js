b68a9c2668dac9427a475e3639ca89744dc7ae8a
atefe-aa <aa.asgariyan12@gmail.com>
2023-08-14 21:33:58 +0330

    make a custom hook for fetching movies

--- src/09-usePopcorn/App.js
+++ src/09-usePopcorn/App.js
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import "./css/index.css";
-import { Loader } from "./components/Loader";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { Navbar } from "./components/Navbar";
-import { Search } from "./components/Search";
-import { Logo } from "./components/Logo";
-import { NumResult } from "./components/NumResult";
-import { Main } from "./components/Main";
-import { Box } from "./components/Box";
-import { MovieList } from "./components/MovieList";
-import { MovieDetailes } from "./components/MovieDetailes";
-import { WatchedSummery } from "./components/WatchedSummery";
-import { WatchedMoviesList } from "./components/WatchedMoviesList";
+import  Loader  from "./components/Loader";
+import  ErrorMessage  from "./components/ErrorMessage";
+import  Navbar  from "./components/Navbar";
+import  Search  from "./components/Search";
+import  Logo  from "./components/Logo";
+import  NumResult  from "./components/NumResult";
+import  Main  from "./components/Main";
+import  Box  from "./components/Box";
+import  MovieList  from "./components/MovieList";
+import  MovieDetailes  from "./components/MovieDetailes";
+import  WatchedSummery  from "./components/WatchedSummery";
+import  WatchedMoviesList  from "./components/WatchedMoviesList";
+import { useMovies } from "./customHooks/useMovies";
 
 document.title = `usePopcorn`;
 
@@ -22,9 +23,6 @@ export const key = "9b708f19";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
   //const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(function () {
@@ -32,6 +30,7 @@ export default function App() {
     const storedValue = localStorage.getItem("watched");
     return JSON.parse(storedValue);
   });
+  const { movies, isLoading, error } = useMovies(query);
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -54,53 +53,6 @@ export default function App() {
       localStorage.setItem("watched", JSON.stringify(watched));
     },
     [watched]
-  );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-      async function moviesFetching() {
-        try {
-          setIsLoading(true);
-          setError("");
-          //to prevent fetching so many requests at the same time
-          const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${key}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Somthing went wrong with fetching movies!");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found!");
-
-          setMovies(data.Search);
-          setError(""); //because of rerendering and abort errors
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            // to ignor abort errors
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          //to have the is loading off,after everything is done specially after throwing an error
-          setIsLoading(false);
-        }
-      }
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-      handleCloseMovie();
-      moviesFetching();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
   );
 
   return (
